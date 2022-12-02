b09c82845d9850e722dd93b814c34fee9a072238
Najwa Ainaya <ainayanajwa26@gmail.com>
2022-12-02 10:28:10 +0700

    update Models

--- app/Models/Fasilitas.php
+++ app/Models/Fasilitas.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fasilitas extends Model
 {
-    //
+    protected $table = 'fasilitas';
+    public $description = 'Fasilitas';
+
+    public const PERMISSIONS = [
+        'create'        => 'fasilitas create',
+        'read'          => 'fasilitas read',
+        'update'        => 'fasilitas update',
+        'delete'        => 'fasilitas delete',
+    ];
+
+    protected $fillable = [
+        'nama_fasilitas',
+        'deskripsi',
+        'harga',
+
+    ];
+
+    // public function periode()
+    // {
+    //     return $this->belongsTo(\App\Models\Training\TrainingPeriode::class, 'training_periode_id');
+    // }
+
+    // public function file()
+    // {
+    //     return $this->belongsTo(\App\Models\Training\Teaching\TeachingMaterial::class, 'teaching_material_id');
+    // }
 }
