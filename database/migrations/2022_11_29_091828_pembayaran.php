0a6b0ad39e0e345325079c6e37439f6bc6bdf31a
Suciizzati28 <suci_izzati28@student.uns.ac.id>
2022-12-02 21:21:58 +0700

    create migration pembayaran

--- database/migrations/2022_11_29_091828_pembayaran.php
+++ database/migrations/2022_11_29_091828_pembayaran.php
@@ -13,7 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('pembayaran', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('total_pembayaran');
+            $table->string('status_pembayan');
+            $table->string('kode_pembayan');
+        });
     }
 
     /**
