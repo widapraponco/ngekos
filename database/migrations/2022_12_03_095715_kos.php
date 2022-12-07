b8eef94f1cd3930f5d9fc27f520083961d7670a4
syahharbanu <Syahharbanu@student.uns.ac.id>
2022-12-07 21:06:23 +0700

    update MigrationKos

--- database/migrations/2022_12_03_095715_kos.php
+++ database/migrations/2022_12_03_095715_kos.php
@@ -24,6 +24,7 @@ return new class extends Migration
             $table->image('gambar1');
             $table->image('gambar2');
             $table->image('gambar3');
+            $table->image('gambar4');
         });
     }
 
