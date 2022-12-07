5670f68feb754d3dbf16ec80b08a5e327c977d69
syahharbanu <Syahharbanu@student.uns.ac.id>
2022-12-07 20:52:10 +0700

    update MigrationKos

--- database/migrations/2022_12_03_095715_kos.php
+++ database/migrations/2022_12_03_095715_kos.php
@@ -13,7 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('kos', function (Blueprint $table) {
+            $table->id();
+        });
     }
 
     /**
