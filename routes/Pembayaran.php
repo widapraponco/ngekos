04170dd736eda1eeb4f59cd9da1489be273d3e75
Suciizzati28 <suci_izzati28@student.uns.ac.id>
2022-12-07 18:38:32 +0700

    create route

--- routes/Pembayaran.php
+++ routes/Pembayaran.php
@@ -2,12 +2,75 @@
 
 $router->group(
     [
+        'namespace' => 'Pembayaran',
         'as' => 'pembayaran',
     ],
     function () use ($router) {
-        $router->post('/pembayaran', ['middleware' => ['auth'], 'uses'=> 'PembayaranController@create',]);
-        $router->get('/pembayaran', ['uses' => 'PembayaranController@find',]);
-        $router->get('/pembayaran/{id}', ['middleware' => ['auth'], 'uses' => 'PembayaranController@findById',]);
-        $router->put('/pembayaran/{id}', ['middleware' => ['auth'], 'uses'=> 'PembayaranController@update',]);
+        $router->group(
+            [
+                'prefix' => 'pembayaran',
+            ],
+            function () use ($router) {
+                // deletes
+                $router->get(
+                    '/deleted',
+                    [
+                        'as' => 'deleted',
+                        'uses' => 'PembayaranDeleteController@deleted',
+                    ]
+                );
+                $router->put(
+                    '/{id}/restore',
+                    [
+                        'as' => 'restore',
+                        'uses' => 'PembayaranDeleteController@restore',
+                    ]
+                );
+                $router->delete(
+                    '/{id}/purge',
+                    [
+                        'as' => 'purge',
+                        'uses' => 'PembayaranDeleteController@purge',
+                    ]
+                );
+
+                // resources
+                $router->get(
+                    '/',
+                    [
+                        'as' => 'index',
+                        'uses' => 'PembayaranController@index',
+                    ]
+                );
+                $router->post(
+                    '/',
+                    [
+                        'as' => 'store',
+                        'uses' => 'PembayaranController@store',
+                    ]
+                );
+                $router->get(
+                    '/{id}',
+                    [
+                        'as' => 'show',
+                        'uses' => 'PembayaranController@show',
+                    ]
+                );
+                $router->put(
+                    '/{id}',
+                    [
+                        'as' => 'update',
+                        'uses' => 'PembayaranController@update',
+                    ]
+                );
+                $router->delete(
+                    '/{id}',
+                    [
+                        'as' => 'destroy',
+                        'uses' => 'PembayaranController@destroy',
+                    ]
+                );
+            }
+        );
     }
 );
