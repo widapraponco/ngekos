cb9c56c1ee8c7b8ced8ba935b33274f7e10ea73b
Suciizzati28 <suci_izzati28@student.uns.ac.id>
2022-12-08 21:56:42 +0700

    update route

--- routes/v1/v1.php
+++ routes/v1/v1.php
@@ -11,5 +11,6 @@ $router->group(
         include 'localization.php';
         include 'frontend/frontend.php';
         include 'backend/backend.php';
+        include 'backend/pembayaran.php';
     }
 );
