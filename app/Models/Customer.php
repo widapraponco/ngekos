727d55c9486600f17777fd5f643fa5f84a060fd3
PerwiraDhani <perwira.dzakwan11@gmail.com>
2022-12-08 09:39:40 +0700

    update models

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,8 +3,40 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+// use App\Models\Traits\Auditable;
 
 class Customer extends Model
 {
-    //
+    // use Auditable;
+    
+    protected $table = 'customer';
+    public $description = 'Customer Kos';
+
+    public const PERMISSIONS = [
+        'create'        => 'customer create',
+        'read'          => 'customer read',
+        'update'        => 'customer update',
+        'delete'        => 'customer delete',
+    ];
+
+    protected $fillable = [
+        'nama_cs',
+        'email_cs',
+        'pass_cs',
+        'foto_cs',
+        'alamat_cs',
+        'notip_cs',
+        'gender',
+        'nik'
+    ];
+
+    // public function periode()
+    // {
+    //     return $this->belongsTo(\App\Models\Training\TrainingPeriode::class, 'training_periode_id');
+    // }
+
+    // public function file()
+    // {
+    //     return $this->belongsTo(\App\Models\Training\Teaching\TeachingMaterial::class, 'teaching_material_id');
+    // }
 }
