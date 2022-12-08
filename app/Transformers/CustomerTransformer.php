6621e3a79db4f9c7cd8dfc33814d6f1ba3a9090a
PerwiraDhani <perwira.dzakwan11@gmail.com>
2022-12-09 06:35:01 +0700

    update Transformer

--- app/Transformers/CustomerTransformer.php
+++ app/Transformers/CustomerTransformer.php
@@ -2,9 +2,11 @@
 
 namespace App\Transformers;
 
+use App\Models\Customer;
 use League\Fractal\TransformerAbstract;
+use App\Transformers\BaseTransformer;
 
-class CustomerTransformer extends TransformerAbstract
+class CustomerTransformer extends BaseTransformer
 {
     /**
      * List of resources to automatically include
@@ -29,10 +31,23 @@ class CustomerTransformer extends TransformerAbstract
      *
      * @return array
      */
-    public function transform()
+    public function transform(Customer $customer)
     {
-        return [
-            //
+        $response = [
+            'id' => self::forId($customer),
+            'nama_cs' => $customer->nama_cs,
+            'email_cs' => $customer->email_cs,
+            'pass_cs' => $customer->pass_cs,
+            'foto_cs' => $customer->foto_cs,
+            'alamat_cs' => $customer->alamat_cs,
+            'notip_cs' => $customer->notip_cs,
         ];
+        return $response;
+    }
+
+    /** @return string */
+    public function getResourceKey(): string
+    {
+        return 'customer';
     }
 }
