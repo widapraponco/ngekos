c4ae11999f1471b13873ecee76fe7f4c8de51162
Suciizzati28 <suci_izzati28@student.uns.ac.id>
2022-12-08 21:54:21 +0700

    update controller

--- app/Http/Controllers/PembayaranController.php
+++ app/Http/Controllers/PembayaranController.php
@@ -7,6 +7,7 @@ use App\Models\Pembayaran;
 use App\Transformers\PembayaranTransformer;
 use Illuminate\Http\Request;
 use Spatie\QueryBuilder\QueryBuilder;
+use Illuminate\Validation\Rule;
 
 class PembayaranController extends Controller
 {
