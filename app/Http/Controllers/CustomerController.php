3c729f588fb203072e63dbb00d34fa49ddb0948a
PerwiraDhani <perwira.dzakwan11@gmail.com>
2022-12-09 06:52:31 +0700

    update Controller

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -44,4 +44,95 @@ class CustomerController extends Controller
             new CustomerTransformer()
         );
     }
+
+    /**
+     *
+     * @OA\Post(
+     *     path="/customer/add-new-customer",
+     *     summary="Add new customer",
+     *     tags={"Customer"},
+     *     security={{"passport" : {}}},
+     *     @OA\Parameter(
+     *         name="include",
+     *         in="query",
+     *         required=false,
+     *         @OA\Schema(
+     *             type="array",
+     *             @OA\Items(
+     *                 type="string",
+     *                 enum={"roles", "permissions"},
+     *             )
+     *         )
+     *     ),
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     description="User key",
+     *                     property="user_id",
+     *                     type="int",
+     *                 ),
+     *                 @OA\Property(
+     *                     description="Role key",
+     *                     property="role_id",
+     *                     type="int",
+     *                 ),
+     *                 example={
+     *                     "user_id" : "user-at-usercom",
+     *                     "role_id" : 1
+     *                 }
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/UserTransformer")
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="401",
+     *         description="Unauthorized",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/Error")
+     *         ),
+     *     ),
+     * )
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return \Spatie\Fractal\Fractal
+     * @throws \Illuminate\Validation\ValidationException
+     * @api                {post} /pembayaran/add-new-customer Add new customer
+     * @apiName            add-customer
+     * @apiGroup           Customer
+     * @apiVersion         1.0.0
+     * @apiPermission      Authenticated User
+     * @apiUse             UserResponse
+     * @apiParam {String} user_id User hashed id
+     * @apiParam {String} role_id Role hashed id
+     *
+     */
+    public function create(Request $request)
+    {
+        $attributes = $this->validate(
+            $request,
+            [
+                'nama_cs' => 'required|string',
+                'email_cs' => 'required|string',
+                'pass_cs' => 'required|string',
+            ]
+        );
+
+        $user = app(FindUserByRouteKeyAction::class)
+            ->execute($attributes['user_id']);
+
+        $user->assignRole($attributes['role_id']);
+
+        return $this->fractal($user->refresh(), new CustomerTransformer());
+    }
 }
