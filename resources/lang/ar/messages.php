8128ad39f341dc2807250ddbbf0c08243bd03020
haasstt <nurafiifalmasazhr@gmail.com>
2022-12-08 20:19:16 +0700

    update documentation

--- resources/lang/ar/messages.php
+++ resources/lang/ar/messages.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 return [
 
     'welcome' => env('APP_NAME', 'Welcome to Lumen Boilerplate').'أهلا بك في ',
--- resources/lang/es/messages.php
+++ resources/lang/es/messages.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 return [
 
     'welcome' => 'Bienvenido a Lumen Boilerplate',
