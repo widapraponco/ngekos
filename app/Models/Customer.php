b1264fe6e991560213880787dbf162247fbab2ae
PerwiraDhani <perwira.dzakwan11@gmail.com>
2022-12-08 09:56:16 +0700

    update model fix

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,25 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-// use App\Models\Traits\Auditable;
 
 class Customer extends Model
 {
-<<<<<<< HEAD
-    // use Auditable;
-    
     protected $table = 'customer';
     public $description = 'Customer Kos';
 
-=======
->>>>>>> e60db765ee6660a0f4603f97533a6138ddcb084f
     public const PERMISSIONS = [
         'create'        => 'customer create',
         'read'          => 'customer read',
         'update'        => 'customer update',
         'delete'        => 'customer delete',
     ];
-<<<<<<< HEAD
 
     protected $fillable = [
         'nama_cs',
@@ -33,16 +26,4 @@ class Customer extends Model
         'gender',
         'nik'
     ];
-
-    // public function periode()
-    // {
-    //     return $this->belongsTo(\App\Models\Training\TrainingPeriode::class, 'training_periode_id');
-    // }
-
-    // public function file()
-    // {
-    //     return $this->belongsTo(\App\Models\Training\Teaching\TeachingMaterial::class, 'teaching_material_id');
-    // }
-=======
->>>>>>> e60db765ee6660a0f4603f97533a6138ddcb084f
 }
