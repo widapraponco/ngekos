b36bc37f9f2c20e7e8e30e2b6fa2a0ad498c21c8
haasstt <nurafiifalmasazhr@gmail.com>
2022-12-08 21:30:38 +0700

    update model admin

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -16,25 +16,9 @@ class Admin extends Model
  * App\Models\Auth\User\SocialAccount
  *
  * @property int $id
- * @property int $user_id
- * @property string $provider
- * @property string $provider_id
- * @property string|null $token
- * @property string|null $avatar
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \App\Models\Auth\User\User $user
- * @method static Builder|SocialAccount newModelQuery()
- * @method static Builder|SocialAccount newQuery()
- * @method static Builder|SocialAccount query()
- * @method static Builder|SocialAccount whereAvatar($value)
- * @method static Builder|SocialAccount whereCreatedAt($value)
- * @method static Builder|SocialAccount whereId($value)
- * @method static Builder|SocialAccount whereProvider($value)
- * @method static Builder|SocialAccount whereProviderId($value)
- * @method static Builder|SocialAccount whereToken($value)
- * @method static Builder|SocialAccount whereUpdatedAt($value)
- * @method static Builder|SocialAccount whereUserId($value)
+ * @property int $nama_admin
+ * @property string $email
+ * @property string $password
  * @mixin \Eloquent
  */
 {
