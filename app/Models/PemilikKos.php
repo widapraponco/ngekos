1caf7b56e0c0e1767656c3f94f5c128f49efee1a
Syauqi <syauqisnh@gmail.com>
2022-12-11 15:16:47 +0700

    update Model PemilikKos

--- app/Models/PemilikKos.php
+++ app/Models/PemilikKos.php
@@ -4,7 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class PemilikKosModel extends Model
+class PemilikKos extends Model
 {
-    //
+    protected $table = 'pemilik kos';
+    public $description = 'pemilik kos';
+
+    public const PERMISSIONS = [
+        'create'        => 'pemilik kos create',
+        'read'          => 'pemilik kos read',
+        'update'        => 'pemilik kos update',
+        'delete'        => 'pemilik kos delete',
+    ];
+
+    protected $fillable = [
+        'nama_pk',
+        'email_pk',
+        'pass_pk',
+        'foto_pk',
+        'alamat_pk',
+        'notip_pk',
+        'jenis_kelamin',
+        'nik'
+    ];
 }
