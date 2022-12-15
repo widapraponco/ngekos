cbde066a4da0d3d9021015140d66e801938f120e
nadifaifani <nadifaifani@gmail.com>
2022-12-15 13:07:09 +0700

    update Admin Controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Admin;
+use Illuminate\Http\Request;
 use App\Transformers\AdminTransformer;
 
 class AdminController extends Controller
