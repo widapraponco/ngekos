a4050d05f5527e6c875a605b838a20f0e5f805d1
Rizkiasa <nurafiifalmasazhr@gmail.com>
2022-12-16 08:19:23 +0700

    update controller

---
+++ app/Http/Controllers/ControllerPemesanan.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pemesanan;
+
+class ControllerPemesanan extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Pemesanan::all();
+
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        //
+        Pemesanan::create($request->all());
+
+        return response()->json("Data pemesanna berhasil ditambahkan");
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Petugas  $petugas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        Pemesanan::where('id', $id)->update($request->all());
+        return response()->json("Data petugas berhasil diupdate");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Petugas  $petugas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Pemesanan::where('id', $id)->delete();
+        return response()->json("Data petugas berhasil dihapus");
+    }
+}
