065da68f5ee251840e2c626e77e1357c2b0a48b3
syahharbanu <Syahharbanu@student.uns.ac.id>
2022-12-16 08:32:52 +0700

    upC

--- app/Http/Controllers/kos.php
+++ app/Http/Controllers/kos.php
@@ -9,4 +9,11 @@ use App\Transformers\KosTransformer;
 class KosController extends Controller
 {
     //
+    public function __construct()
+    {
+        $permissions = Kos::PERMISSIONS;
+
+        $this->middleware('permission:'.$permissions['index'], ['only' => 'index']);
+        
+    }
 }
